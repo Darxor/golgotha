6274333375264b66a2cd1c387c80c893e3b78702
creg <christophe.regouby@free.fr>
2020-07-10 13:50:48 +0200

    model download fails

--- inst/tinytest/test-marianmt.R
+++ inst/tinytest/test-marianmt.R
@@ -1,3 +1,6 @@
+# TODO BUG fqils with  Error in py_call_impl(callable, dots$args, dots$keywords) :
+#TypeError: not a string
+# TODO requires Pip install mosestokenizer
 model <- transformer(architecture = "MarianMT", model_name = "Helsinki-NLP/opus-mt-en-ROMANCE")
 expect_true(inherits(model, "Transformer"))
 expect_true(inherits(predict(model, "For any pretrained model the quality of the “pre-training” process is critical. The Facebook researchers used a dataset extracted from the common crawl corpus of 25 languages (CC25) as a subset and balanced with up/down-sampling based on the percentage for each language in CC25.",
